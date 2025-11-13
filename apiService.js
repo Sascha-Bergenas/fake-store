de5b614bf8a6895ef516569b65f4d29c6252672b
Sascha Bergenås <sascha.bergenas@hotmail.com>
2025-11-13 09:17:07 +0100

    Co-authored-by: Björn Boman <gostabohman@gmail.com> Co-authored-by: Isabelle Franzén <isabellea-f@users.noreply.github.com> Co-authored-by: ALYANNA KARLSSON <AnnaYla@users.noreply.github.com> Co-authored-by: Max Mattiasson <maxmattiasson@users.noreply.github.com>

---
+++ apiService.js
@@ -0,0 +1,14 @@
+async function getData() {
+  const url = "https://fakestoreapi.com/products";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
