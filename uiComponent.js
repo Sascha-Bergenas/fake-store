3d33ef55a52cd225ad620c0da7afcaed2550aa9b
Max Mattiasson <max@mattiasson.eu>
2025-11-13 09:36:26 +0100

    fix

--- uiComponent.js
+++ uiComponent.js
@@ -1,5 +1,5 @@
 export async function handleSearch() {
-  const input = document.querySelector("#productList").value.trim();
+  const input = document.querySelector("#search-product").value.trim();
 
   return getData(input);
 }
